5b6fd71b0fe681a8ada58d822c5cdab50ce09aaf
Mirko Curtolo <mirko.curtolo86@gmail.com>
2025-11-12 14:58:18 +0100

    Run license task

--- src/arduino/app_utils/leds.py
+++ src/arduino/app_utils/leds.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Copyright (C) 2025 ARDUINO SA <http://www.arduino.cc>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 from arduino.app_utils import Logger, Bridge
 
 logger = Logger(__name__)
